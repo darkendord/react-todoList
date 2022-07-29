bc94ca38d9b00e699d68630db9be440fa486cabe
adonisrd <darkendord6@gmail.com>
2022-07-29 13:34:25 -0400

    Saving changes to new branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+let num = 0;
+
 function App() {
+const [input, setInput] = useState("")
+
+ const [todos, setTodos] = useState([
+  {id:num++, todo:"Cook dinner"},
+  {id:num++, todo:"Do the laundry"}
+ ])
+
+function handleChange(e){
+setInput(e.target.value)
+}
+
+function addElement(){
+  input && setTodos((prevTodos)=>{
+    return [...prevTodos, {id:num++, todo:input}]
+  })
+  setInput("")
+}
+
+function handleDelete(elId){
+  setTodos(allTodos => allTodos.filter((todo)=> todo.id !== elId))
+}
+
+function enter(e){
+e.charCode === 13 && addElement()
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onKeyPress={(e)=>enter(e)} className="App">
+      <h1>Todo List</h1> 
+      <input 
+      className='input-el' 
+      type="text"
+      value={input} 
+      onChange={(e)=>handleChange(e)}
+      />
+      <button className='todo-btn' 
+      onClick={()=>addElement()}>Add</button>
+      {todos.map((todo)=>{
+        return (
+        <div className='todo-el' 
+        key={todo.id}><h3>{todo.todo}</h3>
+        <button onClick={()=>handleDelete(todo.id)}>Delete</button>
+        </div>)
+      })}
     </div>
   );
 }
